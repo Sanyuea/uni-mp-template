f161bb895eac8605be1497a74a3542c23eff315e
Sanyuea <1170557347@qq.com>
2023-11-18 10:46:04 +0800

    骨架屏

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,44 +1,97 @@
 <template>
   <view class="viewport">
+    <!-- 自定义导航栏 -->
     <CustomNavbar></CustomNavbar>
-    <scroll-view scroll-y class="scroll-view"></scroll-view>
-    <wSwiper :list="bannerList"></wSwiper>
-    <CategoryPanel :list="categoryList"></CategoryPanel>
-    <HotPannel :list="hotList"></HotPannel>
-    <Guess></Guess>
+    <!-- 滚动容器 -->
+    <scroll-view
+      refresher-enabled
+      @refresherrefresh="onRefresherrefresh"
+      :refresher-triggered="isTriggered"
+      @scrolltolower="onScrolltolower"
+      class="scroll-view"
+      scroll-y
+    >
+      <PageSkeleton v-if="isLoading"></PageSkeleton>
+      <template v-else>
+        <wSwiper :list="bannerList"></wSwiper>
+        <!-- 商品分类 -->
+        <CategoryPanel :list="categoryList" />
+        <!-- 热门推荐 -->
+        <HotPannel :list="hotList" />
+        <!-- 猜你喜欢 -->
+        <Guess ref="guessRef" />
+      </template>
+    </scroll-view>
   </view>
 </template>
-
-<script setup lang="ts">
-//@ts-nocheck
-import CustomNavbar from './components/CustomNavbar.vue'
-import CategoryPanel from './components/CategoryPanel.vue'
-import HotPannel from './components/HotPannel.vue'
-import { getHomeBannerAPI } from '@/api/home.api'
-//获取轮播图
+<script lang="ts" setup>
+// @ts-nocheck
+import CustomNavbar from './components/CustomNavbar'
+import CategoryPanel from './components/CategoryPanel'
+import HotPannel from './components/HotPannel'
+import type { GuessInstance } from '@/types/components'
+import PageSkeleton from './components/PageSkeleton.vue'
+// 获取轮播图
 const bannerList = ref<BannerItem[]>([])
+
 const getHomeBannerData = async () => {
   const res = await getHomeBannerAPI()
   bannerList.value = res.result
 }
 
-//获取分类数据
+// 获取分类数据
 const categoryList = ref<CategoryItem[]>([])
 const getHomeCategoryData = async () => {
   const res = await getHomeCategoryAPI()
   categoryList.value = res.result
 }
-
-//获取热门推荐数据
+// 获取热门推荐数据
 const hotList = ref([])
 const getHomeHotData = async () => {
   const res = await getHomeHotAPI()
   hotList.value = res.result
 }
 
+//是否加载中标记
+const isLoading = ref(false)
+
 onLoad(async () => {
   await Promise.all([getHomeBannerData(), getHomeCategoryData(), getHomeHotData()])
 })
-</script>
 
-<style lang="scss" scoped></style>
+// 获取猜你喜欢组件实例
+const guessRef = ref<wGuessInstance>()
+// 滚动触底事件
+const onScrolltolower = () => {
+  guessRef.value?.getMore()
+}
+
+// 下拉刷新状态
+const isTriggered = ref(false)
+// 自定义下拉刷新被触发
+const onRefresherrefresh = async () => {
+  // 开启动画
+  isTriggered.value = true
+  // 重置猜你喜欢组件数据
+  guessRef.value?.resetData() // 加载数据
+  await Promise.all([getHomeBannerData(), getHomeCategoryData(), getHomeHotData()]) // 关闭动画
+  isTriggered.value = false
+}
+</script>
+<style lang="scss">
+page {
+  background-color: #f7f7f7;
+  height: 100%;
+  overflow: hidden;
+}
+/* 设置滚动区高度 */
+.viewport {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.scroll-view {
+  flex: 1;
+  overflow: hidden;
+}
+</style>

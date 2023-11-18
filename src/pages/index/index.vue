ee30f3e419aec52110f0f03f5c5f0d7c9a3f3bed
Sanyuea <1170557347@qq.com>
2023-11-18 10:45:36 +0800

    轮播图&分类&推荐&猜你喜欢

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,15 +1,44 @@
-<script setup lang="ts">
-//
-</script>
-
 <template>
-  <view class="index">index</view>
-  <div class="flex justify-center h-100 items-center">
-    <div class="w-10 h-10 bg-pink-500"></div>
-  </div>
-  <div class="test bg-yellow-200 w-700 h-100"></div>
+  <view class="viewport">
+    <CustomNavbar></CustomNavbar>
+    <scroll-view scroll-y class="scroll-view"></scroll-view>
+    <wSwiper :list="bannerList"></wSwiper>
+    <CategoryPanel :list="categoryList"></CategoryPanel>
+    <HotPannel :list="hotList"></HotPannel>
+    <Guess></Guess>
+  </view>
 </template>
 
-<style lang="scss">
-//
-</style>
+<script setup lang="ts">
+//@ts-nocheck
+import CustomNavbar from './components/CustomNavbar.vue'
+import CategoryPanel from './components/CategoryPanel.vue'
+import HotPannel from './components/HotPannel.vue'
+import { getHomeBannerAPI } from '@/api/home.api'
+//获取轮播图
+const bannerList = ref<BannerItem[]>([])
+const getHomeBannerData = async () => {
+  const res = await getHomeBannerAPI()
+  bannerList.value = res.result
+}
+
+//获取分类数据
+const categoryList = ref<CategoryItem[]>([])
+const getHomeCategoryData = async () => {
+  const res = await getHomeCategoryAPI()
+  categoryList.value = res.result
+}
+
+//获取热门推荐数据
+const hotList = ref([])
+const getHomeHotData = async () => {
+  const res = await getHomeHotAPI()
+  hotList.value = res.result
+}
+
+onLoad(async () => {
+  await Promise.all([getHomeBannerData(), getHomeCategoryData(), getHomeHotData()])
+})
+</script>
+
+<style lang="scss" scoped></style>
